a19ab26fb8db741d280b822fcd5482c27894e855
rokayaamjaad <Rukayaamged733@gmail.com>
2022-10-22 11:33:56 +0300

    finish

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1=1;
+let num2=62;
+const multiply=num1*num2;
+let random=3;
+let num3=36;
+let num4=9;
+const mod=num3/num4;
+const num5 = 20;
+const num6 = 16;
+const num7 = 2;
 
-// Write your code here
+const  max = Math.max(num5, num6, num7);
